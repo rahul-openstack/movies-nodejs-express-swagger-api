44030815993284e8ea3471156ada4ae8e9c5a6dd
rahul-openstack <rahul-openstack@gmail.com>
2017-04-20 00:47:59 +0530

    Revert "swagger changes"
    This reverts commit 3cb622dad5dcafd75e8f6e33fd79031bf5c2275c.
    
    # Conflicts:
    #	app/routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -22,7 +22,7 @@ var swaggerDefinition = {
         version: '1.0.0',
         description: 'APIs exposed for the Platform : Movies Project',
     },
-    host: 'heroku-node-movies-api.herokuapp.com',
+    host: config.get('app:domain') + ':' + config.get('app:port'),
     basePath: '/'
 };
 
